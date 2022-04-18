a3addb607a86e7eef368cd209b5b0cd358c6880f
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2022-04-18 18:17:16 +0200

    update bot to use new api.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use dotenv::dotenv;
 use hyper::{body::Bytes, StatusCode};
-use image::Rgb;
 use once_cell::sync::Lazy;
-use sus::sus1;
-use std::{collections::HashMap, convert::TryInto, error::Error, fs::{self, File}, sync::Arc};
 
-use serenity::{Result as SerenityResult, async_trait, client::{Client, Context, EventHandler}, framework::standard::CommandError, utils::MessageBuilder, framework::{
+use std::{collections::HashMap, convert::TryInto, error::Error, fs::{self, File}, process::Command, sync::Arc, time::Duration, thread, io::ErrorKind};
+
+use serenity::{Result as SerenityResult, async_trait, 
+    client::{Client, Context, EventHandler}, 
+    framework::standard::CommandError, 
+    utils::MessageBuilder, 
+    framework::{
         StandardFramework,
         standard::{
             Args, CommandResult,
@@ -14,100 +17,34 @@ use serenity::{Result as SerenityResult, async_trait, client::{Client, Context, 
     }, model::{channel::Message, id::UserId}, prelude::*};
 
 use hyper::{body, Body, Client as HyperClient, Method, Request};
+use hyper_tls::HttpsConnector;
 use std::{
     self, 
     io::prelude::*,
     env
 };
+
+use uuid::Uuid;
 use uwuifier::uwuify_str_sse;
 
+use serde::{Serialize, Deserialize};
 use songbird::{
     input::{
         self,
         cached::Memory,
-    },
-    SerenityInit,
+    }, SerenityInit,
 };
-
-
-mod sus;
-
-const C: Rgb<u8> = Rgb([197, 17, 17]);
-const C2: Rgb<u8> = Rgb([122, 8, 56]);
 
 static VOICES: Lazy<HashMap<String, String>> = Lazy::new(|| {
     let mut m = HashMap::new();
-    m.insert("altman".to_string(), "sam-altman".to_string());
-    m.insert("arnold".to_string(), "arnold-schwarzenegger".to_string());
-    m.insert("attenborough".to_string(), "david-attenborough".to_string());
-    m.insert("ayoade".to_string(), "richard-ayoade".to_string());
-    m.insert("bart".to_string(), "bart-simpson".to_string());
-    m.insert("ben_stein".to_string(), "ben-stein".to_string());
-    m.insert("betty_white".to_string(), "betty-white".to_string());
-    m.insert("bill_clinton".to_string(), "bill-clinton".to_string());
-    m.insert("bill_gates".to_string(), "bill-gates".to_string());
-    m.insert("bill_nye".to_string(), "bill-nye".to_string());
-    m.insert("bob_barker".to_string(), "bob-barker".to_string());
-    m.insert("boss".to_string(), "the-boss".to_string());
-    m.insert("brimley".to_string(), "wilford-brimley".to_string());
-    m.insert("broomstick".to_string(), "boomstick".to_string());
-    m.insert("bush".to_string(), "george-w-bush".to_string());
-    m.insert("carter".to_string(), "jimmy-carter".to_string());
-    m.insert("christopher_lee".to_string(), "christopher-lee".to_string());
-    m.insert("cooper".to_string(), "anderson-cooper".to_string());
-    m.insert("craig_ferguson".to_string(), "craig-ferguson".to_string());
-    m.insert("cramer".to_string(), "jim-cramer".to_string());
-    m.insert("cranston".to_string(), "bryan-cranston".to_string());
-    m.insert("crypt_keeper".to_string(), "crypt-keeper".to_string());
-    m.insert("darth_vader".to_string(), "james-earl-jones".to_string());
-    m.insert("david_cross".to_string(), "david-cross".to_string());
-    m.insert("degrasse".to_string(), "neil-degrasse-tyson".to_string());
-    m.insert("dench".to_string(), "judi-dench".to_string());
-    m.insert("devito".to_string(), "danny-devito".to_string());
-    m.insert("dr_phil".to_string(), "dr-phil-mcgraw".to_string());
-    m.insert("earl_jones".to_string(), "james-earl-jones".to_string());
-    m.insert("fred_rogers".to_string(), "fred-rogers".to_string());
-    m.insert("gottfried".to_string(), "gilbert-gottfried".to_string());
-    m.insert("goku".to_string(), "goku".to_string());
-    m.insert("hillary_clinton".to_string(), "hillary-clinton".to_string());
-    m.insert("homer".to_string(), "homer-simpson".to_string());
-    m.insert("krabs".to_string(), "mr-krabs".to_string());
-    m.insert("larry_king".to_string(), "larry-king".to_string());
-    m.insert("lisa".to_string(), "lisa-simpson".to_string());
-    m.insert("luckey".to_string(), "palmer-luckey".to_string());
-    m.insert("mcconnell".to_string(), "mitch-mcconnell".to_string());
-    m.insert("nimoy".to_string(), "leonard-nimoy".to_string());
-    m.insert("nixon".to_string(), "richard-nixon".to_string());
-    m.insert("obama".to_string(), "barack-obama".to_string());
-    m.insert("oliver".to_string(), "john-oliver".to_string());
-    m.insert("palin".to_string(), "sarah-palin".to_string());
-    m.insert("paul_graham".to_string(), "paul-graham".to_string());
-    m.insert("paula_deen".to_string(), "paula-deen".to_string());
-    m.insert("penguinz0".to_string(), "moistcr1tikal".to_string());
-    m.insert("reagan".to_string(), "ronald-reagan".to_string());
-    m.insert("rickman".to_string(), "alan-rickman".to_string());
-    m.insert("rosen".to_string(), "michael-rosen".to_string());
-    m.insert("saruman".to_string(), "christopher-lee".to_string());
-    m.insert("scout".to_string(), "scout".to_string());
-    m.insert("shapiro".to_string(), "ben-shapiro".to_string());
-    m.insert("shohreh".to_string(), "shohreh-aghdashloo".to_string());
-    m.insert("simmons".to_string(), "j-k-simmons".to_string());
-    m.insert("snake".to_string(), "solid-snake".to_string());
-    m.insert("snape".to_string(), "severus-snape".to_string());
-    m.insert("sonic".to_string(), "sonic".to_string());
-    m.insert("spongebob".to_string(), "spongebob-squarepants".to_string());
-    m.insert("squidward".to_string(), "squidward".to_string());
-    m.insert("takei".to_string(), "george-takei".to_string());
-    m.insert("thiel".to_string(), "peter-thiel".to_string());
-    m.insert("trevor".to_string(), "trevor-philips".to_string());
-    m.insert("trump".to_string(), "donald-trump".to_string());
-    m.insert("tucker".to_string(), "tucker-carlson".to_string());
-    m.insert("tupac".to_string(), "tupac-shakur".to_string());
-    m.insert("vegeta".to_string(), "vegeta".to_string());
-    m.insert("wiseau".to_string(), "tommy-wiseau".to_string());
-    m.insert("wizard".to_string(), "wizard".to_string());
-    m.insert("yugi".to_string(), "yami-yugi".to_string());
-    m.insert("zuckerberg".to_string(), "mark-zuckerberg".to_string());
+    m.insert("mario".to_string(), "TM:7wbtjphx8h8v".to_string());
+    m.insert("frieza".to_string(), "TM:pgdraamqpbke".to_string());
+    m.insert("weirdal".to_string(), "TM:rawayafee2a2".to_string());
+    m.insert("moistcr1tikal".to_string(), "TM:k8sj9ghnd3ss".to_string());
+    m.insert("sonic".to_string(), "TM:g21ykeqza9zs".to_string());
+    m.insert("spongebob".to_string(), "TM:h2x7azcafxhh".to_string());
+    m.insert("ben-shapiro".to_string(), "TM:nqwew67rzwz4".to_string());
+    m.insert("bernie".to_string(), "TM:vafvg4nskr59".to_string());
 
     m
 });
@@ -198,20 +135,29 @@ async fn join(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
-async fn sus(ctx: &Context, msg: &Message) -> CommandResult {
+async fn sus(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     if let Some(file) = msg.attachments.first() {
+        let ty = args.single::<u32>().unwrap_or(15);
+        if ty > 50 {
+            check_msg(msg.channel_id.say(ctx, "Please keep dimension under 50").await);
+            return Ok(());
+        }
         let name = file.filename.clone();
         let downloaded = file.download().await?;
         let mut file = File::create(&name).unwrap();
+        let gif = "xd.gif";
 
         file.write_all(&downloaded).unwrap();
-        let gif = sus1(&name).unwrap();
 
-        let files = vec![gif.as_str()];
+        let _ = Command::new("./among-us-dumpy")
+            .args(&[&ty.to_string(), &name, gif])
+            .output();
+
+        let files = vec![gif];
 
         msg.channel_id.send_files(ctx, files, |m| m.content("created gif")).await?;
-        fs::remove_file(&name);
-        fs::remove_file(&gif);
+        fs::remove_file(&name).unwrap();
+        fs::remove_file(&gif).unwrap();
             
     } else {
         check_msg(msg.channel_id.say(ctx, "Please give a file").await);
@@ -224,12 +170,12 @@ async fn sus(ctx: &Context, msg: &Message) -> CommandResult {
 async fn voices(ctx: &Context, msg: &Message) -> CommandResult {
     let mut voices = String::new();
 
-    for (k, v) in VOICES.iter() {
-        voices += &format!("{}:   {}\n", k, v);
+    for k in VOICES.keys() {
+        voices += &format!("\t{}\n", k);
     }
 
     let response = MessageBuilder::new()
-        .push_bold_line("The available voices and their keywords are:")
+        .push_bold_line("The available voices are:")
         .push(voices)
         .build();
 
@@ -260,8 +206,8 @@ async fn clear(ctx: &Context, msg: &Message, cmd_name: &str, _error: Result<(), 
                 Err(_) => {
                     sources.remove(&msg.author.id);
                 }
-            }              
-        }     
+            }
+        }
     }
 }
 
@@ -323,8 +269,10 @@ async fn say(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         let sentence = args.rest();
 
         if let Some(passing_name) = VOICES.get(&name) {
-            let (code, audio) = get_wav_file(passing_name, sentence).await.unwrap();
-            
+            let (code, audio) = get_wav_file(passing_name, sentence)
+                .await
+                .unwrap_or((StatusCode::BAD_REQUEST, Default::default()));
+
             if code == StatusCode::OK {
                 let mut buffer = File::create(filename.clone())?;
                 buffer.write_all(&audio)?;
@@ -339,8 +287,7 @@ async fn say(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
                 let _song = handler.play_source(audio.try_into().unwrap());
                 check_msg(msg.channel_id.say(&ctx.http, "Playing!").await);
             } else {
-                println!("{}, via {}", code, name);
-                check_msg(msg.channel_id.say(&ctx.http, "Couldn't retrieve file from server!").await);
+                check_msg(msg.channel_id.say(&ctx.http, "Couldn't start inference job!").await);
             }
             
         } else {
@@ -368,18 +315,111 @@ fn check_msg(result: SerenityResult<Message>) {
 }
 
 async fn get_wav_file(speaker: &str, text: &str) -> Result<(StatusCode, Bytes), Box<dyn Error>> {
+    let uuid = Uuid::new_v4();
+    let request = TtsInferenceRequest::new(uuid, speaker, text);
     let req = Request::builder()
         .method(Method::POST)
-        .uri("http://mumble.stream/speak")
+        .uri("https://api.fakeyou.com/tts/inference")
         .header("content-type", "application/json")
         .header("accept", "application/json")
-        .body(Body::from(format!("{{\"speaker\": \"{}\", \"text\": \"{}\"}}", speaker, text)))?;
+        .body(Body::from(serde_json::to_string(&request)?))?;
 
-    let client = HyperClient::new();
+    let https = HttpsConnector::new();
+    let client = HyperClient::builder().build::<_, hyper::Body>(https);
+    let res = client.request(req).await?;
+
+    let body_bytes = body::to_bytes(res.into_body()).await?;
+    let deserialized: TtsInferenceResponse = serde_json::from_str(std::str::from_utf8(&body_bytes)?)?;
+    if !deserialized.is_success() {
+        return Err(Box::new(std::io::Error::new(ErrorKind::Other, "Job start was not succesful".to_string())));
+    }
+
+    let mut code: StatusCode = StatusCode::OK;
+
+    let polled =  loop {
+        thread::sleep(Duration::from_secs(3));
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri(format!("https://api.fakeyou.com/tts/job/{}", deserialized.inference_job_token.as_ref().unwrap()))
+            .header("accept", "application/json")
+            .body(Body::empty())?;
+        let res = client.request(req).await?;
+        code = res.status();
+        let body_bytes = body::to_bytes(res.into_body()).await?;
+        let deserialized: TtsJobResult = serde_json::from_str(std::str::from_utf8(&body_bytes)?)?;
+        println!("{:#?}", deserialized);
+        if deserialized.state.status == "complete_success" {
+            break deserialized;
+        }
+    };
+    
+    let req = Request::builder()
+            .method(Method::GET)
+            .uri(format!("https://storage.googleapis.com/vocodes-public{}", polled.state.maybe_public_bucket_wav_audio_path.unwrap()))
+            .header("accept", "application/json")
+            .body(Body::empty())?;
     let res = client.request(req).await?;
     let code = res.status();
     let body_bytes = body::to_bytes(res.into_body()).await?;
-
+    
     Ok((code, body_bytes))
 } 
 
+#[derive(Serialize, Deserialize, Debug)]
+struct TtsInferenceRequest<'a> {
+    uuid_idempotency_token: Uuid,
+    tts_model_token: &'a str,
+    inference_text: &'a str
+}
+
+impl TtsInferenceRequest<'_> {
+    fn new<'a>(uuid_idempotency_token: Uuid, tts_model_token: &'a str, inference_text: &'a str) -> TtsInferenceRequest<'a> {
+        TtsInferenceRequest {
+            uuid_idempotency_token,
+            tts_model_token,
+            inference_text
+        }
+    }    
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TtsInferenceResponse {
+    success: bool,
+    inference_job_token: Option<String>
+}
+
+impl TtsInferenceResponse {
+    fn new() -> TtsInferenceResponse {
+        TtsInferenceResponse {
+            success: false,
+            inference_job_token: None
+        }
+    }
+
+    fn is_success(&self) -> bool {
+        self.success
+    }
+}
+    
+
+#[derive(Serialize, Deserialize, Debug)]
+struct State {
+    job_token: String,
+    status: String,
+    maybe_extra_status_description: Option<String>,
+    attempt_count: u32,
+    maybe_result_token: Option<String>,
+    pub maybe_public_bucket_wav_audio_path: Option<String>,
+    model_token: String,
+    tts_model_type: String,
+    title: String,
+    raw_inference_text: String,
+    created_at: String,
+    updated_at: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TtsJobResult {
+    success: bool,
+    pub state: State
+}
